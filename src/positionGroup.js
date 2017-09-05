b8979eddcaee2b573eb5013c4c64fdd95583ab3a
Rick Lupton <mail@ricklupton.name>
2017-09-05 15:33:32 +0100

    Remove use of Object.assign()
    (modifies group object in-place now)

--- src/positionGroup.js
+++ src/positionGroup.js
@@ -1,19 +1,20 @@
-export default function positionGroup(nodes, group) {
+export default function positionGroup (nodes, group) {
   const rect = {
     top: Number.MAX_VALUE,
     left: Number.MAX_VALUE,
     bottom: 0,
     right: 0
-  };
+  }
 
   group.nodes.forEach(n => {
-    const node = nodes.get(n);
-    if (!node) return;
-    if (node.x < rect.left) rect.left = node.x;
-    if (node.x > rect.right) rect.right = node.x;
-    if (node.y < rect.top) rect.top = node.y;
-    if (node.y + node.dy > rect.bottom) rect.bottom = node.y + node.dy;
-  });
+    const node = nodes.get(n)
+    if (!node) return
+    if (node.x < rect.left) rect.left = node.x
+    if (node.x > rect.right) rect.right = node.x
+    if (node.y < rect.top) rect.top = node.y
+    if (node.y + node.dy > rect.bottom) rect.bottom = node.y + node.dy
+  })
 
-  return Object.assign({}, group, { rect });
+  group.rect = rect
+  return group
 }
